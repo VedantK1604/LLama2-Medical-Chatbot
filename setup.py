62108e41467d6a6875b92e2a8b5c68c15bfb5156
Vedant <khapekarvedant@gmail.com>
2025-01-16 17:02:34 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Langchain Medical Chatbot Project',
+    version= '0.0.0',
+    author= 'Vedant Khapekar',
+    author_email= 'vedantkhapekar16@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
